ec9a95405bf3aecd5b5d024937a88393b90383f7
DutraNeto <dudutraneto@gmail.com>
2020-01-20 02:09:57 -0500

    fix accordion method -- remove onmouseevents -- add IIFE to method

--- 01-basic/assets/scripts/accordion.js
+++ 01-basic/assets/scripts/accordion.js
@@ -1,12 +1,13 @@
 // toggle accordion function for hiding checkboxes
-function toggleAccordion (evt) {
+(function toggleAccordion () {
     let acc = document.querySelectorAll(".accordion")
     let accordion = Array.from(acc)
     accordion.forEach(element => {
-        element.addEventListener("click", function() {
+        element.addEventListener("click", function(e) {
+            e.preventDefault()
             this.classList.toggle("active")
-            let panel = element.nextElementSibling;
+            let panel = this.nextElementSibling;
             panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + "px"
         })
     })
-}
+})()
