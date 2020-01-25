d21155ee22407284b8724c6095102d2c293f4051
DutraNeto <dudutraneto@gmail.com>
2020-01-25 18:36:46 -0500

    write resetFilter function thar resets all inputs

--- 01-basic/assets/scripts/update-price-labels.js
+++ 01-basic/assets/scripts/update-price-labels.js
@@ -2,7 +2,7 @@
 function updatePriceLabels() {
     const ranges = document.querySelectorAll(".shopping-list__range input")
     const labels = document.querySelectorAll(".shopping-list__range .shopping-list__range-price label")
-    
+
     // const minValue = parseInt(ranges[0].min)
     // const maxValue = parseInt(ranges[0].max)
     let price1 = parseInt(ranges[0].value)
@@ -16,3 +16,27 @@ function updatePriceLabels() {
     labels[0].innerHTML = "$ " + price1
     labels[1].innerHTML = "$ " + price2
 }
+
+(function resetFilters() {
+    const reset = document.getElementsByClassName("filter-btn")[0]
+    const ranges = document.querySelectorAll(".shopping-list__range input")
+    const labels = document.querySelectorAll(".shopping-list__range .shopping-list__range-price label")
+    const inputSearch = document.getElementById("search-brand")
+    const controls = document.querySelectorAll(".checkbox-control input")
+    reset.addEventListener("click", function(e) {
+        // do not refresh
+        e.preventDefault()
+        // reset ranges
+        // ranges[0].value = "0"
+        // ranges[1].value = "10000"
+        labels[0].innerHTML = `$ ${ranges[0].value = 0}`
+        labels[1].innerHTML = `$ ${ranges[1].value = 10000}`
+        // reset input search
+        inputSearch.value = ""
+        // reset all checkboxes
+        const arrControl = [...controls]
+        arrControl.forEach(control => {
+            control.checked = false
+        });
+    })
+})()
