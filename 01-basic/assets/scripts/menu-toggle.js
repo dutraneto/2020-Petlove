a6d2be78174a2cdcd82c984771d4e3bdcdb66282
DutraNeto <dudutraneto@gmail.com>
2020-02-24 22:33:27 -0500

    prevent scrolling on activated menu

--- 01-basic/assets/scripts/menu-toggle.js
+++ 01-basic/assets/scripts/menu-toggle.js
@@ -1,5 +1,6 @@
 // toggle menu
 (function menuToggle () {
+    let body = document.body
     let menuBars = document.getElementsByClassName("menu-open")[0]
     let navMobile = document.getElementsByClassName("navbar-mobile")[0]
     let menuClose = document.getElementsByClassName("menu-close")[0]
@@ -7,10 +8,12 @@
     menuBars.addEventListener("click", function(e) {
         e.preventDefault()
         navMobile.classList.add("open")
+        body.classList.toggle("no-scroll")
     })
     menuClose.addEventListener("click", function(e) {
         e.preventDefault()
         this.parentElement.classList.remove("open")
+        body.classList.toggle("no-scroll")
     })
 
 })()
