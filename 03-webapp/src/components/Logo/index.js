8e22fe949c919ddf1650789590f3d974af385b09
JDutra <dutraneto@me.com>
2020-08-09 13:48:06 -0400

    Add Link to Logo

--- 03-webapp/src/components/Logo/index.js
+++ 03-webapp/src/components/Logo/index.js
@@ -1,15 +1,19 @@
 import * as S from './styled'
 
+import Link from 'next/link'
+
 const Logo = (props) => (
-    <S.LogoWrapper href='./' title='Home page'>
-        <S.Logo
-            src='/images/petlove-logo.png'
-            loading='lazy'
-            width='226'
-            height='61'
-            alt='Site Logo'
-        />
-    </S.LogoWrapper>
+    <Link href='./' passHref>
+        <S.LogoWrapper title='Home page'>
+            <S.Logo
+                src='/images/petlove-logo.png'
+                loading='lazy'
+                width='226'
+                height='61'
+                alt='Site Logo'
+            />
+        </S.LogoWrapper>
+    </Link>
 )
 
 export default Logo
