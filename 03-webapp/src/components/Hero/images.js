c8b96203e75d5d15bdd11f9ae4f9fe241b124ccb
JDutra <dutraneto@me.com>
2020-08-28 11:13:56 -0400

    Add props to head component as well as head files

--- 03-webapp/src/components/Hero/images.js
+++ 03-webapp/src/components/Hero/images.js
@@ -1,6 +1,6 @@
 const images = [
-    { src: '/images/hero-bg.png', desc: 'Picture of a cat on snow' },
-    { src: '/images/hero-bg-1.png', desc: 'Picture of dog with santa costume' },
+    { src: '/images/hero-bg.jpg', desc: 'Picture of a cat on snow' },
+    { src: '/images/hero-bg-1.jpg', desc: 'Picture of dog with santa costume' },
     { src: '/images/hero-bg-2.jpg', desc: 'Picture of puppy running on grass' },
     { src: '/images/hero-bg-3.jpg', desc: 'Picture of a blue beta fish' },
     { src: '/images/hero-bg-4.jpg', desc: 'Picture of dog with Captain America costume' },
--- 03-webapp/src/pages/_app.js
+++ 03-webapp/src/pages/_app.js
@@ -8,17 +8,57 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                <noscript>
+                    <div className='alert alert-danger' role='alert'>
+                        You need to enable JavaScript to run this app.
+                    </div>
+                </noscript>
+                <meta charSet='utf-8' />
+                <meta
+                    name='viewport'
+                    content='width=device-width, initial-scale=1, viewport-fit=cover'
+                />
+                <meta httpEquiv='X-UA-Compatible' content='ie=edge' />
                 <title>Petlove</title>
                 <link rel='shortcut icon' href='/images/favicon.png' />
                 <link rel='apple-touch-icon' href='/images/tile-512X512.png' />
                 <link rel='apple-touch-icon' href='/images/tile-192X192.png' />
+                <meta name='description' content='Your smartest way to buy pet stuffs' />
+                <meta name=' keywords' content='e-commerce, pets' />
+                <meta name='robots' content='index, follow' />
+                <meta name='apple-mobile-web-app-capable' content='yes' />
+                <meta name='apple-mobile-web-app-status-bar-style' content='black' />
+                <meta name='theme-color' content='black' />
+                <meta name='msapplication-config' content='browserconfig.xml' />
+                <link rel='canonical' href='' />
+                <link rel='manifest' href='/manifest.json' />
+                <meta property='og:type' content='website' />
+                <meta property='og:url' content='https://petlove.com' />
+                <meta property='og:title' content='PetLove' />
+                <meta property='og:image' content='' />
+                <meta property='og:description' content='Your smartest way to buy pet stuffs' />
                 <meta
-                    name='description'
-                    content='Project starter for Petlove 2020 using React, NextJs and styled components'
+                    property='og:site_name'
+                    content='PetLove - Your smartest way to buy pet stuffs'
                 />
-
+                <meta property='og:locale' content='en_US' />
+                <meta property='og:image:width' content='1200' />
+                <meta property='og:image:height' content='630' />
+                <meta name='twitter:card' content='summary' />
+                <meta name='twitter:site' content='@petlove' />
+                <meta name='twitter:creator' content='@glacial' />
+                <meta name='twitter:url' content='https://petlove.com' />
+                <meta name='twitter:title' content='Your smartest way to buy pet stuffs' />
+                <meta
+                    name='twitter:description'
+                    content='PetLove - Your smartest way to buy pet stuffs'
+                />
+                <meta
+                    name='twitter:image'
+                    content='http://graphics8.nytimes.com/images/2011/12/08/technology/bits-newtwitter/bits-newtwitter-tmagArticle.jpg'
+                />
                 <link
-                    href='https://fonts.googleapis.com/css?family=Lato:400,700|Montserrat:300,500,700|Unna:400,700'
+                    href='https://fonts.googleapis.com/css?family=Lato:400,700|Montserrat:300,500,700|Unna:400,700&display=swap'
                     rel='stylesheet'
                 />
             </Head>
